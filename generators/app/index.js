43c8940e41a1ab319b4b483dc2dcb1b0ffdb2f6e
Unai Recio <unai.recio@agcpartners.co.uk>
2015-11-23 11:19:22 +0100

    feat(seed): Adds appName variable into templates context + cleans bower

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,28 +6,10 @@ module.exports = yeoman.generators.Base.extend({
   prompting: function () {
     var done = this.async();
 
-// TODO: finish package json (including app name on the file, author, licese and repositoryUrl)
-// TODO: include a bower file
-// TODO: include a gruntfile. See http://yeoman.io/authoring/gruntfile.html
-// TODO: finish bootstrap inclusion on the other files + dependency. http://yeoman.io/authoring/dependencies.html
-// TODO: allow arguments, etc
-// TODO: ask for an optional repositoryUrl
-// TODO: put in the readme that it's translations ready + json server to mock + integration tests
-// TODO: also readme. Explain that inside the styleguide-only the scss won't get included on the main.css
-// TODO: remove aws from gruntfile
-// TODO: Figure out how the user could configure travis and put that in the docs
-// TODO: translations: remove veruscript file and leave it blank
-// TODO: remove any veruscript/daad trace
-
     // Have Yeoman greet the user.
     utils.welcome.call(this);
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'bootstrap',
-      message: 'Do you want to include bootstrap-sass?',
-      default: true
-    }];
+    var prompts = [];
     if(!this.config.get('appName')) {
       prompts.push(utils.promptAppName);
     }
@@ -42,7 +24,8 @@ module.exports = yeoman.generators.Base.extend({
   writing: function () {
     this.fs.copyTpl(
       this.templatePath('./**/*'),
-      this.destinationPath('./')
+      this.destinationPath('./'),
+      {appName: this.props.appName}
     );
   },
 
