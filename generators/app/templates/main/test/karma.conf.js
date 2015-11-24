756d6f5af28a0fe9bdb5a278eecd9efbffde9fd0
Unai Recio <unai.recio@agcpartners.co.uk>
2015-11-24 20:23:20 +0100

    fix(karma conf generator): Removes extra excludes

--- generators/app/templates/main/test/karma.conf.js
+++ generators/app/templates/main/test/karma.conf.js
@@ -71,13 +71,8 @@ module.exports = function(config) {
 
     // list of files / patterns to exclude
     exclude: [
-      'app/**/{,*/}*.pageobject.js',
-      'app/**/{,*/}*.scenario.js',
-      'app/components/about/{*,*/}*',
       'app/assets/**/*',
-      'app/common/app/*',
-      'app/components/upload/**/*',
-      'app/components/usermenu/**/*'
+      'app/common/app/*'
     ],
 
     // web server port
@@ -115,11 +110,11 @@ module.exports = function(config) {
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-//    Uncomment the following lines if you are using grunt's server to run the tests
-//    proxies: {
-//       '/': 'http://localhost:8081/'
-//    },
-//    URL root prevent conflicts with the site root
-//    urlRoot: '_karma_'
+    //    Uncomment the following lines if you are using grunt's server to run the tests
+    //    proxies: {
+    //       '/': 'http://localhost:8081/'
+    //    },
+    //    URL root prevent conflicts with the site root
+    //    urlRoot: '_karma_'
   });
 };
